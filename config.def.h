b235146a6843785da36f1751d174f500c916bd8b
Samyak Puri <samyakpuri@hotmail.com>
2020-04-13 00:21:33 +0200

    added mouse scrollback

--- config.def.h
+++ config.def.h
@@ -203,8 +203,14 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* button               mask            string */
-	{ Button4,              XK_ANY_MOD,     "\031" },
-	{ Button5,              XK_ANY_MOD,     "\005" },
+	{ Button4,              XK_NO_MOD,      "" },
+	{ Button5,              XK_NO_MOD,      "" },
+};
+
+MouseKey mkeys[] = {
+	/* button               mask            function        argument */
+	{ Button4,              ShiftMask,      kscrollup,      {.i =  1} },
+	{ Button5,              ShiftMask,      kscrolldown,    {.i =  1} },
 };
 
 /* Internal keyboard shortcuts. */
